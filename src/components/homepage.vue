ec17b84104c77b39abebc2b56376610182464e4e
Sam Clayton <sam.clayton@audacia.co.uk>
2026-06-01 12:37:12 +0100

    feat: added animated loading spinner and example button

--- src/components/homepage.vue
+++ src/components/homepage.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Loader from '../components/loader.vue'
+</script>
 
 <template>
   <div class="image-container">
     <h2>There is no content to see here.</h2>
     <img src="../img/Screenshot 2026-05-28 171557.png" />
+    <Loader />
   </div>
 </template>
 
@@ -11,11 +14,18 @@
 h2 {
   color: white;
   text-align: center;
+  margin: 0;
+  padding: 0;
 }
 
 .image-container {
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
+}
+
+.image-container img {
+  padding: 2rem;
 }
 </style>
---
+++ src/components/loader.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isLoading = ref(false)
+
+const setLoadingFalse = () => {
+  isLoading.value = false
+}
+
+const setLoadingTrue = () => {
+  isLoading.value = true
+}
+
+const fakeLoadTime = () => {
+  setLoadingTrue()
+  setTimeout(() => {
+    setLoadingFalse()
+  }, 2000)
+}
+</script>
+
+<template>
+  <button @click="fakeLoadTime">Load Animation Test</button>
+  <div v-if="isLoading" class="loader">
+    <img src="../img/cat-face-spin-removebg.png" />
+  </div>
+</template>
+
+<style scoped>
+.loader {
+  display: flex;
+  justify-content: center;
+}
+
+.loader img {
+  width: 100%;
+  height: 100px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
